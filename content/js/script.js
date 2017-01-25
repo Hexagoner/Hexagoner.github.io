e92ff6dff373c9987b35a22d45fd1181b466d3fc
Stylish Web <farzad247@gmail.com>
2017-01-25 16:08:43 +0000

    Updated the js and css

--- content/js/script.js
+++ content/js/script.js
@@ -1,10 +1,21 @@
-﻿/*
+﻿/*  ****************************************
     App
-*/
+    ****************************************    */
 
 var app = app || {};
 
+app.page = {
+    default: "page-default",
+    protect: "page-protect",
+    find: "page-find",
+    offers: "page-offers",
+    health: "page-health",
+    crime: "page-crime",
+    tracker: "page-tracker"
+}
+
 app.DOM = {
+    root: "root",
     header: "header",
     content: "content",
     button: {
@@ -13,8 +24,67 @@ app.DOM = {
         notification: "button-notification",
         info: "button-info",
         filter: "button-filter"
-    }
+    },
+    page: app.page
 }
+
+app.dimension = {
+    document: {
+        width: $(document).width(),
+        height: $(document).height()
+    },
+    window: {
+        width: $(window).width(),
+        height: $(window).height()
+    },
+    header: {
+        width: $('#' + app.DOM.header).width(),
+        height: $('#' + app.DOM.header).height()
+    },
+    content: {
+        width: $('#' + app.DOM.content).width(),
+        height: $('#' + app.DOM.content).height()
+    },
+}
+
+app.method = app.method || {};
+
+
+/*  ****************************************
+    Activate Page
+    ****************************************    */
+app.method.activatePage = function () {
+
+    // Loop through objects in app.page
+    // see if any of those exists as class for html tag
+    // if so, display that page by removing hidden class for its corrispondent under app.dom.page
+
+    var activePage = '';
+
+    //console.log(activePage)
+
+    //setInterval(function () {
+    //    for (var key in app.page) {
+    //        var value = app.page[key];
+
+    //        //console.log(document.getElementById(value));
+    //    }
+    //}, 2000);
+}
+
+
+/*  ****************************************
+    ****************************************    */
+
+/*  ****************************************
+    ****************************************    */
+
+/*  ****************************************
+    ****************************************    */
+
+/*  ****************************************
+    ****************************************    */
+
 
 // Header buttons
 var headerButtons = function () {
@@ -43,9 +113,13 @@ var headerButtons = function () {
 
 app.init = function () {
 
+    console.log(app.page, app.DOM.page)
+
     console.log('Init...')
 
     headerButtons();
+
+    app.method.activatePage();
 
 };
 
@@ -56,17 +130,33 @@ window.onload = function() {
 
 
 /*
+    \\\\\\\\\\\\\\\\\\\\////////////////////
+*/
+
+
+/*
     Lock the screen orientation to portrait mode
 */
-$(document).ready(function () {
-    function reorient(e) {
-        var portrait = (window.orientation % 180 == 0);
-        $("#root").css("-webkit-transform", !portrait ? "rotate(-90deg)" : "");
+function doOnOrientationChange() {
+    switch (window.orientation) {
+        case -90:
+        case 90:
+            $('html').removeClass('orientation-portrait').addClass('orientation-landscape');
+            break;
+        default:
+            $('html').removeClass('orientation-landscape').addClass('orientation-portrait');
+            break;
     }
-    window.onorientationchange = reorient;
-    window.setTimeout(reorient, 0);
-});
+}
 
+window.addEventListener('orientationchange', doOnOrientationChange);
+
+// Initial execution if needed
+doOnOrientationChange();
+
+/*
+    \\\\\\\\\\\\\\\\\\\\////////////////////
+*/
 
 /*
     Add header background when page scrolls
@@ -86,3 +176,7 @@ $(function () {
     window.addEventListener('scroll', fun);
 
 });
+
+/*
+    \\\\\\\\\\\\\\\\\\\\////////////////////
+*/
